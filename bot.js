df03b155990f18674a23a838f453849a63ca5621
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2018-12-09 06:40:22 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,36 +47,6 @@ client.on('ready', () => {
 
 
 
-client.on('message' , message => {
-if (message.content === '$help') {
-           if (!message.member.hasPermission('MANAGE_MESSAGES')) return;
-         let embed = new Discord.RichEmbed()
-
-      .setThumbnail(message.author.avatarURL)    
-      .addField("**۩ஜ▬▬▬▬▬▬✦ (أوامر البوت (عام✦▬▬▬▬▬▬ஜ۩**","** **")
-    .addField("**۩ஜ▬▬▬▬▬▬✦ (أوامر البوت (عام✦▬▬▬▬▬▬ஜ۩**","** **")
-     .addField("**❖ +ban**","**لتبنيد شخص من السيرفر**")
-      .addField("**❖ +kick**","**لطرد شخص من السيرفر**")
-      .addField("**❖ +mute**","**لإعطاء ميوت لشخص**")
-      .addField("**❖ +say**","**يردد كلامك **")
-      .addField("**❖ +bc**","**لإرسال برودكاست لجميع اعضاء السيرفر**")
-      .addField("**❖ +new**","**لفتح تذكره**")
-      .addField("**❖ +close**","**لإقفال التذكره**")
-      .addField("**❖ +server**","**معلومات عن السيرفر**")
-      .addField("**❖ +ping**","**لرؤية بنق البوت**")
-      .addField("**❖ +SetName**","**لتغيير اسم البوت**")
-      .addField("**❖ +Stream**","**لتغيير ستريمنق البوت**")
-      .addField("**❖ +Playing**","**لتغيير بلاينق البوت**")
-      .addField("**❖ +Listening**","**لتغيير لستنق البوت**")
-      .addField("**❖ +Watching**","**لتغيير واتشنق البوت**")
-      .addField("**❖ +ccolor<Number>**","**لصنع الوان مع عددهم**")
-      .addField("**❖ +setcolor<Number>**","**لإختيار اللون**")
-      .addField("**۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩**","** **")
-
-    .setColor('#e9ab26')
-  message.author.sendEmbed(embed);
-    }
-});
  
   
 
@@ -86,21 +56,6 @@ if (message.content === '$help') {
 
 
 
-client.on('message' , message => {
-if (message.content === '$inv') {
-           if (!message.member.hasPermission('MANAGE_MESSAGES')) return;
-         let embed = new Discord.RichEmbed()
-
-      .setThumbnail(message.author.avatarURL)    
-      .addField("**۩ஜ▬▬▬▬▬▬✦ (أوامر البوت (عام✦▬▬▬▬▬▬ஜ۩**","** **")
-      .addField("**۩ஜhttps://discordapp.com/api/oauth2/authorize?client_id=500704774677856266&permissions=8&scope=botஜ۩**","** **")
-      .addField("**۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩**","** **")
-
-    .setColor('RANDOM')
-  message.author.sendEmbed(embed);
-    }
-});
-  
 
 
 
