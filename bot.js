47560cc40c7e0c47cc34722d2a362b08825b6d1c
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2018-12-09 07:17:37 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
-const fs = require("fs"); 
 const dateFormat = require('dateformat'); 
-const jimp = require('jimp');   
-var prefix = "$"
+const prefix = "+"
 const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
