99f62b1d31201fc124328e3d16f8e6c8129e07bb
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2018-12-09 09:40:36 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -377,6 +377,51 @@ setInterval(function(){})
 
 
 
+client.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+  let args = message.content.split(" ").slice(1);
+
+if (command == "Esay") {
+
+    let say = new Discord.RichEmbed()
+
+    .setDescription(args.join(" "))
+
+    .setColor(0x23b2d6)
+
+    message.channel.sendEmbed(say);
+
+    message.delete();
+
+  }
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
