2910fc10a0c71c389ee600747c909c1cd49440f1
Mdax <44174101+Mdax77xR1@users.noreply.github.com>
2018-12-09 06:48:52 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 const Discord = require('discord.js');
 const fs = require("fs"); 
 const jimp = require('jimp');   
-var prefix = "+"
+var prefix = "$"
 const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -28,5 +28,22 @@ client.on('ready', () => {
   console.log('╚[════════════]╝')
   console.log('')
   console.log('')
+});
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Welcome Mdax`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     xR1Server' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`$help $inv`,"https://www.twitch.tv/Mdax77xR1")
+client.user.setStatus("online")
+ 
+});
+
+
 
 client.login(process.env.BOT_TOKEN);
